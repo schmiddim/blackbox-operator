0cc36d5d375fc52fd126d95432a573c93d328543
schmiddim <schmiddim@gmx.at>
2025-03-30 12:58:47 +0200

    e2e tests are a little too dangerous for my kubectl config

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -16,17 +16,17 @@ limitations under the License.
 
 package e2e
 
-import (
-	"fmt"
-	"testing"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-)
-
-// Run e2e tests using the Ginkgo runner.
-func TestE2E(t *testing.T) {
-	RegisterFailHandler(Fail)
-	_, _ = fmt.Fprintf(GinkgoWriter, "Starting blackbox-operator suite\n")
-	RunSpecs(t, "e2e suite")
-}
+//import (
+//	"fmt"
+//	"testing"
+//
+//	. "github.com/onsi/ginkgo/v2"
+//	. "github.com/onsi/gomega"
+//)
+//
+//// Run e2e tests using the Ginkgo runner.
+//func TestE2E(t *testing.T) {
+//	RegisterFailHandler(Fail)
+//	_, _ = fmt.Fprintf(GinkgoWriter, "Starting blackbox-operator suite\n")
+//	RunSpecs(t, "e2e suite")
+//}
